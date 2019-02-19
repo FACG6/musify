6ec7d4ad74227d5222e245ea83ed0794717341b8
alaabadra4 <alaabadra4@gmail.com>
2019-02-19 16:27:55 +0200

    deleting queries

--- src/queries/addSong.js
+++
@@ -1,5 +0,0 @@
-const connection = require('../database/db_coonction.js');
-const insertData = connection.query(`INSERT INTO USERS ( ) VALUES ${}` ,(error ,res)=>{
-    if(error) cb (error);
-    cb(null ,res);
-});
--- src/queries/getSongs.js
+++
@@ -1,6 +0,0 @@
-const connection = require('../database/db_coonction.js');
-const getData = connection.query('select * from songs',(error ,res)=>{
-    if(error) cb(error);
-    cb(null ,res.rows);
-})
-module.exports = getData;
