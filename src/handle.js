5b017c3e922a89ad9ea5da346b347880a92f175b
mohammedmh <mohammedabumahadi@gmail.com>
2019-02-19 14:17:30 +0200

    create the basic of handler just for testing relates

---
+++ src/handle.js
@@ -0,0 +1,27 @@
+const handleHomePage =() =>{
+
+}
+const handleSignup =() =>{
+
+}
+const handleLogin =() =>{
+
+}
+const handleStatic = () =>{
+
+}
+const handleSongs = () =>{
+    
+}
+const handleAddsong = () =>{
+
+}
+const handleMyfav = () =>{
+
+}
+const handleNotFound = () =>{
+
+}
+const handleNotFound =() =>{
+
+}
