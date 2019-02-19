5a71ad2e4f32a433773022cd511136c024480649
Israa <israa96sulaiman@gmail.com>
2019-02-19 15:00:29 +0200

    queries

---
+++ src/handler.js
@@ -0,0 +1,39 @@
+const handleHomePage =() =>{
+
+}
+const handleSignup =() =>{
+
+}
+const handleLogin =() =>{
+
+}
+const handleStatic = () =>{
+
+}
+const handleSongs = () =>{
+    
+}
+const handleAddsong = () =>{
+
+}
+const handleMyfav = () =>{
+
+}
+const handleServerError = () =>{
+
+}
+const handleNotFound =() =>{
+
+}
+
+module.exports = {
+  handleHomePage,
+  handleSignup,
+  handleNotFound,
+  handleServerError,
+  handleMyfav,
+  handleStatic,
+  handleLogin,
+  handleAddsong,
+  handleSongs
+}
---
+++ src/queries/addFav.js
@@ -0,0 +1,8 @@
+const connect = require('../database/db_connection');
+
+const addFav = (userId, callback) => {
+    const text = 'INSERT INTO songs  ;
+    const value = [userId];
+
+
+}
