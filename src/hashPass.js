f856d6fd84ab4a69d949ae4994243d785c51ae3d
Israa <israa96sulaiman@gmail.com>
2019-02-20 09:03:21 +0200

    src/handler.js

--- src/hashPass.js
+++ src/hashPass.js
@@ -1,16 +1,19 @@
 const crypt = require('bcrypt');
 
 const hashPassword = (password) => {
-  crypt.genSaltSync(10, (err, salt) => {
-    return crypt.hashSync(password, salt);
-  });
+  return crypt.hashSync(password, 10);
 }
 
-const comparePassword = (password, hashed, callback)=>{
-    crypt.compare(password, hashed, callback);
+// console.log(hashPassword('123'));
+
+const comparePassword = (password, hashed, callback) => {
+  crypt.compare(password, hashed, callback);
 }
 
 module.exports = {
   hashPassword,
   comparePassword
 }
+// comparePassword('123', '$2b$10$lAMDQGpBY013cY/gw9NkFedELrm2X4YGwWOFlSIp0HGS6n.kxyfju', (err, res)=>{
+//   console.log(res);
+// })
